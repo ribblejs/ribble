624ad626c646b3894603f1c1b44cadf4f3c9612a
devdigital <devdigital.cloud@gmail.com>
2020-02-04 22:56:15 +0000

    chore: reset mode on story change

--- packages/pulse/src/Decorator.js
+++ packages/pulse/src/Decorator.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
 import { addons, makeDecorator } from '@storybook/addons'
 import { STORY_CHANGED } from '@storybook/core-events'
-import { useAddonState, useChannel } from '@storybook/api'
 import { Pulse } from './Pulse'
 
 const Decorator = ({ story }) => {
@@ -11,14 +9,14 @@ const Decorator = ({ story }) => {
   const [channel] = useState(addons.getChannel())
 
   useEffect(() => {
-    channel.emit('pulse/check-status')
+    //channel.emit('pulse/check-status')
 
     const handleModeChange = mode => {
       setMode(mode)
     }
 
-    const handleStoryChange = story => {
-      console.log(story)
+    const handleStoryChange = () => {
+      channel.emit('pulse/reset')
     }
 
     channel.on('pulse/change-mode', handleModeChange)
@@ -32,62 +30,6 @@ const Decorator = ({ story }) => {
 
   return <Pulse mode={mode}>{currentStory}</Pulse> // TODO: options
 }
-
-// class Decorator extends Component {
-//   static propTypes = {
-//     channel: PropTypes.shape({
-//       emit: PropTypes.func,
-//       on: PropTypes.func,
-//       removeListener: PropTypes.func,
-//     }),
-//     story: PropTypes.object.isRequired,
-//     // breakpoints: PropTypes.objectOf(PropTypes.number),
-//   }
-
-//   static defaultProps = {
-//     channel: undefined,
-//     // breakpoints: {
-//     //   tablet: 768,
-//     //   desktop: 1024,
-//     // },
-//   }
-
-//   constructor(props) {
-//     super(props)
-
-//     const { channel, story } = props
-
-//     if (channel) {
-//       this.channel = channel
-//     } else {
-//       this.channel = addons.getChannel()
-//     }
-
-//     this.story = story
-//   }
-
-//   state = {
-//     mode: 'paused',
-//   }
-
-//   componentDidMount() {
-//     this.channel.emit('pulse/check-status')
-
-//     this.channel.on('pulse/change-mode', mode => {
-//       this.setState({ mode })
-//     })
-//   }
-
-//   componentDidUpdate(prevProps) {
-//     if (this.props.story !== prevProps.story) {
-//       this.story = this.props.story
-//     }
-//   }
-
-//   render() {
-//     return <Pulse mode={this.state.mode}>{this.story}</Pulse> // TODO: options
-//   }
-// }
 
 export const withPulse = makeDecorator({
   name: 'withPulse',
--- packages/pulse/src/Tool.js
+++ packages/pulse/src/Tool.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Icons, IconButton } from '@storybook/components'
+import { STORY_CHANGED } from '@storybook/core-events'
 
 export class Tool extends Component {
   static propTypes = {
@@ -32,18 +33,19 @@ export class Tool extends Component {
   componentDidMount() {
     const { channel } = this.props
 
-    channel.on('pulse/check-status', () => {
-      channel.emit('pulse/change-mode', this.state.mode)
-    })
+    // channel.on('pulse/reset', () => {
+    //   this.setState('paused', () =>
+    //     this.channel.emit('pulse/change-mode', 'paused')
+    //   )
+    // })
+
+    channel.on(STORY_CHANGED, () => this.setMode('paused'))
   }
 
-  setMode() {
-    this.setState(
-      { mode: this.state.mode === 'paused' ? 'playing' : 'paused' },
-      () => {
-        this.props.channel.emit('pulse/change-mode', this.state.mode)
-      }
-    )
+  setMode(mode) {
+    this.setState({ mode }, () => {
+      this.props.channel.emit('pulse/change-mode', mode)
+    })
   }
 
   render() {
@@ -51,7 +53,9 @@ export class Tool extends Component {
       <IconButton
         key="control"
         title={this.state.mode === 'paused' ? 'Play' : 'Pause'}
-        onClick={this.setMode}
+        onClick={() =>
+          this.setMode(this.state.mode === 'paused' ? 'playing' : 'paused')
+        }
         mode={this.state.mode}
       >
         <Icons icon={this.state.mode === 'paused' ? 'arrowright' : 'cross'} />
