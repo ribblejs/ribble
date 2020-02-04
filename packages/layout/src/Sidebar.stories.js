c4c67e92173ce80bd685676a774c4b4d0ed1f9ee
devdigital <devdigital.cloud@gmail.com>
2020-02-04 22:59:08 +0000

    chore: update sidebar stories

--- packages/layout/src/Sidebar.stories.js
+++ packages/layout/src/Sidebar.stories.js
@@ -1,9 +1,7 @@
 /** @jsx jsx */
-import { useState } from 'react'
 import { jsx, Styled, Box } from 'theme-ui'
 import { Sidebar } from './Sidebar'
 import { text, paragraphs } from '@utilz/dummy'
-import { keyframes } from '@emotion/core'
 
 export default {
   title: 'Sidebar',
@@ -28,58 +26,6 @@ const PauseIcon = () => (
   </svg>
 )
 
-const ShrinkGrow = ({ minWidth, maxWidth, timeInSeconds, children }) => {
-  const [state, setState] = useState('paused')
-
-  const squeeze = keyframes({
-    '0%': { width: maxWidth },
-    '100%': { width: minWidth },
-  })
-
-  return (
-    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-      <Box
-        sx={{
-          backgroundColor: '#eee',
-          margin: '0 auto',
-          borderRadius: '50%',
-          cursor: 'pointer',
-          width: '70px',
-          height: '70px',
-          display: 'flex',
-          mb: 2,
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        onClick={() =>
-          state === 'paused' ? setState('playing') : setState('paused')
-        }
-      >
-        <Box sx={{ width: '20px', height: '20px' }}>
-          {state === 'paused' ? <PlayIcon /> : <PauseIcon />}
-        </Box>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box
-          sx={{
-            animation: `${timeInSeconds}s linear infinite alternate ${squeeze}`,
-            animationPlayState: state === 'paused' ? 'paused' : 'running',
-          }}
-        >
-          {children}
-        </Box>
-      </Box>
-    </Box>
-  )
-}
-
-ShrinkGrow.defaultProps = {
-  minWidth: '300px',
-  maxWidth: '100%',
-  timeInSeconds: 5,
-}
-
 const defaultProps = {
   sidebarWidth: '20rem',
   sidebar: (
@@ -98,26 +44,10 @@ const defaultProps = {
   ),
 }
 
-export const withLeftSidebar = () => (
-  <ShrinkGrow timeInSeconds={3}>
-    <Sidebar {...defaultProps} />
-  </ShrinkGrow>
-)
+export const withLeftSidebar = () => <Sidebar {...defaultProps} />
 
-export const withRightSidebar = () => (
-  <ShrinkGrow>
-    <Sidebar {...defaultProps} side="right" />
-  </ShrinkGrow>
-)
+export const withRightSidebar = () => <Sidebar {...defaultProps} side="right" />
 
-export const withNumericGap = () => (
-  <ShrinkGrow>
-    <Sidebar {...defaultProps} gap={3} />
-  </ShrinkGrow>
-)
+export const withNumericGap = () => <Sidebar {...defaultProps} gap={3} />
 
-export const withStringGap = () => (
-  <ShrinkGrow>
-    <Sidebar {...defaultProps} gap="70px" />
-  </ShrinkGrow>
-)
+export const withStringGap = () => <Sidebar {...defaultProps} gap="70px" />
